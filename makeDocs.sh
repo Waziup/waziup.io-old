d1825c8d6336301c2f38c132359880c0b9077028
Corentin Dupont <corentin.dupont@gmail.com>
2020-01-04 14:21:51 +0100

    docs

--- makeDocs.sh
+++ makeDocs.sh
@@ -3,11 +3,12 @@ OUT=static/docs
 
 makePdf () {
   echo "Making $1 ..."
-  cat $DOC/$2/index.md | sed -f replace_shortcodes.sed | pandoc -o $OUT/$1 --template waziup-doc-template.tex  --resource-path $DOC/$2/ --toc
-  #pandoc -o $OUT/$1 --filter test 
+  cat $DOC/$2/index.md | pandoc -t latex -o $OUT/$1.tex --template waziup-doc-template.tex  -V resourcepath=$DOC/$2/ --toc 
+  cat $OUT/$1.tex | sed -f replace_shortcodes.sed > $OUT/$1-rep.tex
+  pdflatex -output-directory=$OUT -jobname=$1 $OUT/$1-rep.tex 
 }
 
-makePdf WaziDev_User_Manual-V1.0.pdf wazidev
-makePdf WaziGate_User_Manual-V1.0.pdf wazigate
-makePdf WaziCloud_User_Manual-V2.1.pdf wazicloud
-makePdf WaziCloud_API_Reference-V2.1.pdf API/V2
+makePdf WaziDev_User_Manual-V1.0 wazidev
+makePdf WaziGate_User_Manual-V1.0 wazigate
+makePdf WaziCloud_User_Manual-V2.1 wazicloud
+makePdf WaziCloud_API_Reference-V2.1 API/V2
