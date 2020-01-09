c8b5ffbe79a8455f5e789b0d47dccd396f3c7ec6
Corentin Dupont <corentin.dupont@gmail.com>
2020-01-09 14:58:25 +0100

    two passes tex

--- makeDocs.sh
+++ makeDocs.sh
@@ -5,6 +5,7 @@ makePdf () {
   echo "Making $1 ..."
   cat $DOC/$2/index.md | pandoc -t latex --template waziup-doc-template.tex -V resourcepath=$DOC/$2/ --toc --listings | sed -f replace_shortcodes.sed > $OUT/$1.tex
   pdflatex -output-directory=$OUT -jobname=$1 $OUT/$1.tex 
+  pdflatex -output-directory=$OUT -jobname=$1 $OUT/$1.tex 
 }
 
 makePdf WaziDev_User_Manual-V1.0 wazidev
