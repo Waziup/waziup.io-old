61a82ad696bc1eadfab287d5d31d9cf0d05c7a52
Corentin Dupont <corentin.dupont@gmail.com>
2020-04-05 18:01:15 +0200

    mobile

--- static/js/app.js
+++ static/js/app.js
@@ -52,6 +52,16 @@ function setAnchor() {
   });
 }
 
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mobile-menu").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mobile-menu").style.width = "0";
+} 
+
 function documentReady() {
 
   setColorMenu();
